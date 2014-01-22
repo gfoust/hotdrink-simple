2fd725cc86b861540f98d6841bbdbaa72a1617a4
Gabriel Foust <gfoust@cse.tamu.edu>
2014-01-22 14:17:41 -0600

    Reworked asynchronous evaluation. Now any method may simply return a promise, in which case evaluation is suspended until the promise is fulfilled. Logic for asynchronous execution on web workers still exists, but it is separate from the property models.

--- support/worker.js
+++ support/worker.js
@@ -2,43 +2,48 @@ var hd= {
   notify: function notify( result ) {
     self.postMessage( {result: result, version: hd.version, complete: false} );
   }
-}
+};
 
-function getter( context, used, key ) {
-  return function() {
-    used[key]= true;
-    return context[key];
-  };
-}
+(function() {
 
-function execute( fn, context ) {
-  var used= {};
-  var proxy= {};
-
-  for (var key in context) {
-    Object.defineProperty( proxy, key,
-                           {enumerable: true,
-                            get: getter( context, used, key )} );
+  function getter( context, used, key ) {
+    return function() {
+      used[key]= true;
+      return context[key];
+    };
   }
 
-  try {
-    var result= fn.call( proxy );
-    return {used: used, result: result, version: hd.version, complete: true};
+  function execute( fn, context ) {
+    var used= {};
+    var proxy= {};
+
+    for (var key in context) {
+      Object.defineProperty( proxy, key,
+                             {enumerable: true,
+                              get: getter( context, used, key )} );
+    }
+
+    try {
+      var result= fn.call( proxy );
+      return {used: used, result: result, complete: true};
+    }
+    catch (e) {
+      return {used: used, error: e.toString(), complete: true};
+    }
   }
-  catch (e) {
-    return {used: used, error: error, version: hd.version, complete: true};
-  }
-}
 
-self.addEventListener( 'message', function( event ) {
+  self.addEventListener( 'message', function( event ) {
 
-  var fnstr= event.data.fn;
-  fnstr= fnstr.substring( fnstr.indexOf( '{' ) + 1,
-                          fnstr.lastIndexOf( '}' ) );
-  var fn= new Function( fnstr );
+    var fnstr= event.data.fn;
+    fnstr= fnstr.substring( fnstr.indexOf( '{' ) + 1,
+                            fnstr.lastIndexOf( '}' ) );
+    var fn= new Function( fnstr );
 
-  hd.version= event.data.version;
-  hd.inputsComplete= event.data.inputsComplete;
+    hd.version= event.data.version;
+    hd.inputsComplete= event.data.inputsComplete;
 
-  self.postMessage( execute( fn, event.data.context ) );
-} );
+    //self.postMessage( {complete: true, result: fnstr} );
+    self.postMessage( execute( fn, event.data.context ) );
+  } );
+
+}());
